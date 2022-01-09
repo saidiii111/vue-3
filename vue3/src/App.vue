3321797b199377834b078002f05fcff73532da3b
Said Saghir <35297641+saidiii111@users.noreply.github.com>
2022-01-09 11:39:17 +0100

    adding v-bind and interpolation with Obj of Arr

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>jjjj</div>
+ <h1>Dc hero</h1>
+    <ul>
+     <li v-for="(hero, index) in dcHeros" v-bind:key="index">
+       {{ index }} : {{ hero.name }}
+       </li>
+    </ul>
 </template>
+
+
+<script>
+export default {
+data(){
+  return {
+    dcHeros : [
+     { name: "SuperGirl" },
+     { name: "SuperGirl" },
+     { name: "SuperGirl" },
+     { name: "SuperGirl" },
+     { name: "SuperGirl" },
+  ],
+};
+},
+};
+</script>
+
+<style>
+body{
+  background: #004400;
+}
+</style>
