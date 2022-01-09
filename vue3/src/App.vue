9aa6c79417d0fd148f6311f940c2534a007303e3
Said Saghir <35297641+saidiii111@users.noreply.github.com>
2022-01-09 22:52:07 +0100

    adding bstrap-5 style

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,16 +1,27 @@
 <template>
-
- <h1>Dc hero {{ herosCount }}</h1>
-    <ul>
-       <li v-for="(hero, index) in dcHeros" :key="hero.name">
-          <div>{{ hero.name }} <button v-on:click="remove(index)">X</button></div>
-       </li>
+<div class="m-auto">
+ <h1 class="text-3xl text-center my-4">Dc Hero {{ herosCount }}</h1>
+    <ul class="border rounded p-2">
+       <li class="flex justify-between" 
+       v-for="(hero, index) in dcHeros" 
+       :key="hero.name"
+       >
+          <div>
+            {{ hero.name }}
+          </div>
+             <button
+             class="border rounded bg-gradient-to-r from-red-700 to-pink-500 text-white ml-2 px-2" v-on:click="remove(index)">X</button>
+        </li>
     </ul>
 
-    <form @submit.prevent="addHero">
-       <input v-model.trim="newHero" placeholder="Type Hero Name here" />
+    <form class="mt-10"
+     @submit.prevent="addHero">
+       <input
+       class="border rounded px-2"
+        v-model.trim="newHero" placeholder="Type Hero Name here" />
        <button class="bg-info" type="submit">Add Hero</button>
     </form>
+    </div>
 </template>
 
 
